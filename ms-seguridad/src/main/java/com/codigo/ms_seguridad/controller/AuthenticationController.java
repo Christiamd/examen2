4e14ee6701b5e4ee67179d28f6f67d29c5d83946
Christiam De la Cruz <christiamdelacruz@hotmail.com>
2024-12-21 06:11:45 -0500

    subiendo archivos

---
+++ ms-seguridad/src/main/java/com/codigo/ms_seguridad/controller/AuthenticationController.java
@@ -0,0 +1,81 @@
+package com.codigo.ms_seguridad.controller;
+
+import com.codigo.ms_seguridad.aggregates.request.SignInRefreshToken;
+import com.codigo.ms_seguridad.aggregates.request.SignInRequest;
+import com.codigo.ms_seguridad.aggregates.request.SignUpRequest;
+import com.codigo.ms_seguridad.aggregates.response.SignInResponse;
+import com.codigo.ms_seguridad.entity.Usuario;
+import com.codigo.ms_seguridad.service.AuthenticationService;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/authentication/v1/")
+@RequiredArgsConstructor
+@RefreshScope
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    //@Value("${dato.propiedad}")
+    private String datoPropiedad="test";
+    @PostMapping("/signupuser")
+    public ResponseEntity<Usuario> signUpUser(
+            @RequestBody SignUpRequest signUpRequest){
+        return ResponseEntity.ok(authenticationService
+                .signUpUser(signUpRequest));
+    }
+    @PostMapping("/signupadmin")
+    public ResponseEntity<Usuario> signUpAdmin(
+            @RequestBody SignUpRequest signUpRequest){
+        return ResponseEntity.ok(authenticationService
+                .signUpAdmin(signUpRequest));
+    }
+
+    //OBTENER UN ACCESS TOKEN POR MEDIO DE LOGIN
+    @PostMapping("/signin")
+    public ResponseEntity<SignInResponse> signIn(
+            @RequestBody SignInRequest signInRequest){
+        return ResponseEntity.ok(authenticationService
+                .signIn(signInRequest));
+    }
+    //OBTENER UN ACCESS TOKEN PO MEDIO DE REFRESHTOKEN
+    @PostMapping("/refreshToken")
+    public ResponseEntity<SignInResponse> signInRefresh(
+            @RequestBody SignInRefreshToken signInRefreshToken){
+        return ResponseEntity.ok(authenticationService
+                .getTokenByRefresh(signInRefreshToken));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Usuario>> getALl(){
+        return ResponseEntity.ok(authenticationService.todos());
+    }
+
+    @GetMapping("/clave")
+    public ResponseEntity<String> getClaveFirma(@RequestHeader("X-Code-App") String valor){
+        String dato;
+        if(valor.equals("Validado")){
+            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+            dato = Base64.getEncoder().encodeToString(key.getEncoded());
+        }else {
+            dato=null;
+        }
+
+        return ResponseEntity.ok(dato);
+    }
+
+    @GetMapping("/prueba")
+    public ResponseEntity<String> getPrueba(){
+        return ResponseEntity.ok(datoPropiedad);
+    }
+}
